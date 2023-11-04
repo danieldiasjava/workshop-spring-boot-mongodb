cd66ce1d90848e55ffe6d9582c0f208bae52a2f5
Daniel Dias <danieldias.devjava@gmail.com>
2023-11-04 09:43:58 -0300

    Obtendo um usuário por id

--- src/main/java/com/danieldias/workshopmongo/services/UserService.java
+++ src/main/java/com/danieldias/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.danieldias.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.danieldias.workshopmongo.domain.User;
 import com.danieldias.workshopmongo.repository.UserRepository;
+import com.danieldias.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,11 @@ public class UserService {
 	public List<User> findAll() {
 
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
 	}
 
 }
