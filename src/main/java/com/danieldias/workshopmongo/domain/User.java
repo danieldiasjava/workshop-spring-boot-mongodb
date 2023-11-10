f6a0fcea9658fcf85fbd1458191b557318c1a52b
Daniel Dias <danieldias.devjava@gmail.com>
2023-11-10 09:11:03 -0300

    Referenciando os posts do usuário

--- src/main/java/com/danieldias/workshopmongo/domain/User.java
+++ src/main/java/com/danieldias/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.danieldias.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -14,6 +17,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> pots = new ArrayList<>();
 	
 	public User() {
 		
@@ -49,6 +55,14 @@ public class User implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPots() {
+		return pots;
+	}
+
+	public void setPots(List<Post> pots) {
+		this.pots = pots;
+	}
 
 	@Override
 	public int hashCode() {
@@ -66,7 +80,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
 }
